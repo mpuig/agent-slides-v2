fef3de6b1f236de6a58575f125a01286cdcba6ee
Marc Puig <marc.puig@gmail.com>
2026-03-28 09:01:49 +0100

    Add slide collection CLI commands

--- src/agent_slides/engine/__init__.py
+++ src/agent_slides/engine/__init__.py
@@ -1,5 +1,6 @@
 """Layout and render engine package."""
 
+from agent_slides.engine.reflow import rebind_slots, reflow_deck
 from agent_slides.engine.text_fit import fit_text
 
-__all__ = ["fit_text"]
+__all__ = ["fit_text", "rebind_slots", "reflow_deck"]
