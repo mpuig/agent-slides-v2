e4f9b7d2218a7787a8b4ed4d13a0274debb1a665
Marc Puig <marc.puig@gmail.com>
2026-03-28 08:51:58 +0100

    Add Pydantic scene graph types (#20)
    Co-authored-by: Puig, Marc <Puig.Marc+bcgprod@bcg.com>

--- src/agent_slides/__init__.py
+++ src/agent_slides/__init__.py
@@ -1,7 +1,7 @@
 """agent-slides package."""
 
-from .errors import AgentSlidesError
+from .errors import AgentSlidesError, INVALID_SLIDE
 
 __version__ = "0.1.0"
 
-__all__ = ["__version__", "AgentSlidesError"]
+__all__ = ["__version__", "AgentSlidesError", "INVALID_SLIDE"]
